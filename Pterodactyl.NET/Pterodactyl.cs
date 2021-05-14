21f84ac5197ddcc26d2362176e9fe63d7c96b84a
Thoo <40025225+thoo0224@users.noreply.github.com>
2021-05-14 20:35:46 +0200

    minor refactoring

--- Pterodactyl.NET/Pterodactyl.cs
+++ Pterodactyl.NET/Pterodactyl.cs
@@ -5,6 +5,7 @@ using Pterodactyl.NET.Exceptions;
 using RestSharp;
 
 using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace Pterodactyl.NET
@@ -16,10 +17,7 @@ namespace Pterodactyl.NET
 
         public static Pterodactyl Instance;
 
-    
-
         public PterodactylV0_7 V0_7;
-
         public PterodactylV1_0 V1_0;
 
         public Pterodactyl(string hostname, string key)
@@ -39,12 +37,16 @@ namespace Pterodactyl.NET
 
             HttpClient = new RestClient(host);
             HttpClient.UseSerializer<NewtonsoftSerializer>();
-            HttpClient.AddDefaultHeader("Authorization", $"Bearer {key}");
-            HttpClient.AddDefaultHeader("Accept", "Application/vnd.pterodactyl.v1+json");
+            HttpClient.AddDefaultHeaders(new Dictionary<string, string>()
+            {
+                { "Authorization", $"Bearer {key}" },
+                { "Accept", "Application/vnd.pterodactyl.v1+json" }
+            });
 
             V0_7 = new PterodactylV0_7(HttpClient);
             V1_0 = new PterodactylV1_0(HttpClient);
             Instance = this;
         }
+
     }
 }
